fddf034458521f75c94a90428a929e62fc772f4a
binomial14 <leowinston2017@gmail.com>
2020-10-14 10:55:43 +0800

    update function

--- src/App.js
+++ src/App.js
@@ -2,23 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {rotate: 1};
+  }
+
+  change = () => {
+    this.setState((state) => ({rotate: -1*state.rotate}));
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <header className="App-header">
+        {this.state.rotate===1 ? <img id="logo" src={logo} className="App-logo" alt="logo" />:<img id="logo" src={logo} className="App-logo-reverse" alt="logo" />}
         <p>B06901032 吳兩原</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <button id="change" className="button" onClick={this.change}>{this.state.rotate===1 ? "不戴口罩":"戴口罩"}</button>
+        <a>
+          {this.state.rotate===1?"順時中":"逆時中"}
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    )
+    
+
+    
+  }
 }
+
+
+
+
 
 export default App;
